858db5645cb395a76751b5a1efb9e4dfc2323e91
John Laffey <laffeychem@gmail.com>
2024-09-02 20:23:24 -0400

    add a little functionality to simulate different timeframes

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 from datetime import datetime, timedelta
 
 end_date = datetime.today()
-start_date = end_date - timedelta(365)
+start_date = end_date - timedelta(
+    365
+    * int(
+        input(
+            "Enter the amount of years you would like to analyze over. (1, 2, 3, etc...)\n"
+        )
+    )
+)
 
 end_date_str = end_date.strftime("%Y-%m-%d")
 start_date_str = start_date.strftime("%Y-%m-%d")
